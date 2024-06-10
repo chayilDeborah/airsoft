5e06d244f02980f38c3b5584d309f41a599b475e
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-06-10 04:32:59 +0100

    feat: Homepage responsiveness

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -12,29 +12,29 @@ import Footer from './Footer'
 const Homepage = () => {
   return (
     <main className=''>
-    <main  className='lg:max-w-[1450px] w-full mx-auto lg:w-full'>
+    <main  className='lg:max-w-[1450px] w-[93%] mx-auto lg:w-full'>
         <Navbar />
-        {/* <section className='flex justify-center mt-[52px] mb-[60px] '>
+        <section className='flex justify-center mt-[52px] mb-[60px] '>
             <div>
             <div>
-                <h3 className='text-[25px] leading-[32.5px] text-[#9D9393] font-medium '>Airsoft Studio</h3>
+                <h3 className='lg:text-[25px] lg:leading-[32.5px] text-[8.5px] leading-[11px] text-[#9D9393] font-medium '>Airsoft Studio</h3>
                 <div className='flex'>
-                    <h1 className='text-[96px] leading-[124.8px] text-[#1B1C1E] w-[811px]  '><i className='font-light text-[#28292c]'>Leading</i> Fantasy Concept Art Studio</h1>
-                    <Image src={birds} alt='birds-img' className='w-[158px] h-[100px] mt-auto ml-[61px] ' />
+                    <h1 className='lg:text-[96px] lg:leading-[124.8px] text-[31.6px] leading-[42.4px] text-[#1B1C1E] lg:w-[811px] w-[329px] '><i className='font-light text-[#28292c]'>Leading</i> Fantasy Concept Art Studio</h1>
+                    <Image src={birds} alt='birds-img' className='lg:w-[158px] lg:h-[100px] w-[54px] h-[34px] mt-auto ml-[20px] lg:ml-[61px] ' />
                 </div>
             </div>
-            <div className='flex justify-between mt-[27px] '>
-                <Image src={superwoman} alt='superwoman' className='' />
-                <div className='w-[82px] mx-[19px] mt-auto h-[82px] mb-[68px] rounded-[50%] border-[2px] flex justify-center border-[#1B1C1E] '>
-                  <Image src={arrow} alt='arrow' className='my-auto mx-auto ' />
+            <div className='lg:flex justify-between mt-[15px] lg:mt-[27px] '>
+                <Image src={superwoman} alt='superwoman' className='lg:w-[511px] lg:h-[304px] w-[334px] mx-auto lg:mx-[0px] ' />
+                <div className=' lg:w-[82px] lg:h-[82px] w-[25px] h-[25px] lg:mx-[19px] mt-[25px] lg:mt-auto lg:mb-[68px] -mb-[25px] rounded-[50%] border-[0.6px] lg:border-[2px] flex justify-center border-[#1B1C1E] '>
+                  <Image src={arrow} alt='arrow' className='my-auto mx-auto lg:w-[25px] w-[7px] ' />
                 </div>
-                <div className='w-[525px] mt-[38px]  '>
-                  <i className='text-[25px] leading-[32.5px] text-[#1B1C1E] font-light  '>&#34;Art is how we decorate space; Music is how we decorate time.&#34; - Jean-Michel Basquiat</i>
-                  <Image src={kingdom} alt='kingdom' className='' />
+                <div className='lg:w-[525px] lg:mt-[38px] flex lg:block '>
+                  <i className='lg:text-[25px] lg:leading-[32.5px] text-[10px] leading-[13px] text-[#1B1C1E] font-light lg:w-[519px] w-[180px] lg:mt-[0px] mt-[30px]  '>&#34;Art is how we decorate space; Music is how we decorate time.&#34; - Jean-Michel Basquiat</i>
+                  <Image src={kingdom} alt='kingdom' className='lg:w-[511px] lg:h-[200px] w-[200px] h-[69px] ' />
                 </div>
             </div>
             </div>
-        </section> */}
+        </section>
        
     </main>
      {/* <PortfolioComponent />
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,8 +15,8 @@ const Navbar = () => {
   };
 
   return (
-    <main className='flex justify-center mt-[36px] lg:mt-[30px] overflow-hidden '>
-      <section className='w-[80%] flex justify-between '>
+    <main className='flex justify-center mt-[20px] lg:mt-[30px] overflow-hidden '>
+      <section className='lg:w-[80%] w-full flex justify-between '>
         <Link href='/'>
           <Image src={logo} alt='logo' className='w-[89.24px] h-[30px] lg:w-[200px] lg:h-[69.8px] mt-[20px] lg:mt-[0px] ' />
         </Link>
