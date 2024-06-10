c22fe37c179f3eb567df559add426f112a094bf0
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-06-10 15:53:03 +0100

    feat: Latestwork responsiveness

--- app/components/LatestWork.tsx
+++ app/components/LatestWork.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 interface CarouselProps {
@@ -7,8 +7,25 @@ interface CarouselProps {
 }
 
 const LatestWork: React.FC<CarouselProps> = ({ images }) => {
-  const itemsPerPage = 4;
+  const [itemsPerPage, setItemsPerPage] = useState(4);
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const updateItemsPerPage = () => {
+      if (window.innerWidth < 768) {
+        setItemsPerPage(2.5);
+      } else {
+        setItemsPerPage(4);
+      }
+    };
+
+    window.addEventListener('resize', updateItemsPerPage);
+    updateItemsPerPage(); // Call once to set the initial value
+
+    return () => {
+      window.removeEventListener('resize', updateItemsPerPage);
+    };
+  }, []);
 
   const handleNext = () => {
     setCurrentIndex((prevIndex) => (prevIndex + itemsPerPage) % images.length);
@@ -19,25 +36,25 @@ const LatestWork: React.FC<CarouselProps> = ({ images }) => {
   };
 
   return (
-    <div className="relative w-[1052px] mx-auto mb-[100px]">
+    <div className="relative lg:w-[1052px] w-[360px] mx-auto mb-[100px]">
       <div className='flex justify-between '>
-       <h2 className="text-[30px] leading-[39px] font-semibold mb-[40px] ">Our Latest Works</h2>
-       <div>
-       <button
-         onClick={handlePrev}
-         className="text-[30px] mr-[5px] "
-       >
-         ←
-       </button>
-       <button
-         onClick={handleNext}
-         className="text-[30px]  "
-       >
-         →
-       </button>
-       </div>
- </div>
-      <div className="flex space-x-4">
+        <h2 className="text-[16.4px] leading-[21.3px] lg:text-[30px] lg:leading-[39px] font-semibold mb-[40px] ">Our Latest Works</h2>
+        <div>
+          <button
+            onClick={handlePrev}
+            className="text-[22px] lg:text-[30px] mr-[5px] "
+          >
+            ←
+          </button>
+          <button
+            onClick={handleNext}
+            className="text-[22px] lg:text-[30px]  "
+          >
+            →
+          </button>
+        </div>
+      </div>
+      <div className="flex space-x-4 overflow-hidden">
         {images.slice(currentIndex, currentIndex + itemsPerPage).map((src, index) => (
           <div key={index} className="flex-none">
             <Image src={src} alt={`Image ${index + 1}`} width={250} height={250} className="object-cover w-[250px] h-[250px] rounded-[30px]" />
