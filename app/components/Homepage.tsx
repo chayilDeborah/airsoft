610688a42503fed6f4f29e973baec8f9bd470989
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-06-10 04:48:25 +0100

    fix

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -19,12 +19,12 @@ const Homepage = () => {
             <div>
                 <h3 className='lg:text-[25px] lg:leading-[32.5px] text-[8.5px] leading-[11px] text-[#9D9393] font-medium '>Airsoft Studio</h3>
                 <div className='flex'>
-                    <h1 className='lg:text-[96px] lg:leading-[124.8px] text-[31.6px] leading-[42.4px] text-[#1B1C1E] lg:w-[811px] w-[329px] '><i className='font-light text-[#28292c]'>Leading</i> Fantasy Concept Art Studio</h1>
+                    <h1 className='lg:text-[96px] lg:leading-[124.8px] text-[31.6px] leading-[42.4px] text-[#1B1C1E] lg:w-[811px] w-[287px] '><i className='font-light text-[#28292c]'>Leading</i> Fantasy Concept Art Studio</h1>
                     <Image src={birds} alt='birds-img' className='lg:w-[158px] lg:h-[100px] w-[54px] h-[34px] mt-auto ml-[20px] lg:ml-[61px] ' />
                 </div>
             </div>
             <div className='lg:flex justify-between mt-[15px] lg:mt-[27px] '>
-                <Image src={superwoman} alt='superwoman' className='lg:w-[511px] lg:h-[304px] w-[334px] mx-auto lg:mx-[0px] ' />
+                <Image src={superwoman} alt='superwoman' className='lg:w-[511px] lg:h-[304px] w-full mx-auto lg:mx-[0px] ' />
                 <div className=' lg:w-[82px] lg:h-[82px] w-[25px] h-[25px] lg:mx-[19px] mt-[25px] lg:mt-auto lg:mb-[68px] -mb-[25px] rounded-[50%] border-[0.6px] lg:border-[2px] flex justify-center border-[#1B1C1E] '>
                   <Image src={arrow} alt='arrow' className='my-auto mx-auto lg:w-[25px] w-[7px] ' />
                 </div>
